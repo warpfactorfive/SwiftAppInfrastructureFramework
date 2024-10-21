e83735516836eef402615c1dcbc5745c44550a9f
warpfactorfive <terry.sd@gmail.com>
2024-10-21 09:56:49 -0700

    Initial Commit

---
+++ SwiftInfrastructureFramework/SwiftInfrastructureFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftInfrastructureFramework.h
+//  SwiftInfrastructureFramework
+//
+//  Created by Terry on 10/21/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftInfrastructureFramework.
+FOUNDATION_EXPORT double SwiftInfrastructureFrameworkVersionNumber;
+
+//! Project version string for SwiftInfrastructureFramework.
+FOUNDATION_EXPORT const unsigned char SwiftInfrastructureFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftInfrastructureFramework/PublicHeader.h>
+
+
